2fc02612089fd5ba5d1e95f3abc1069ff1f76202
kirito <kirito666codr@gmail.com>
2025-05-25 22:56:44 +0530

    feat: update Hero component with layout and content for the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Hero from "@/components/home/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <main>
     <Hero/>
-    </>
+    </main>
   );
 }
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,5 +1,17 @@
 export default function Hero() {
     return (
-        <div></div>
+        <div className="grid grid-cols-1 md:grid-cols-2 ml-10 md:ml-20 mt-20">
+            <div>
+                <h1 className="font-semibold text-5xl">Premium Car rental in your Area</h1>
+                <p className="mt-10 text-gray-500 font-semibold text-2xl max-w-[90%]">Book the selected car effortiessly, Pay for driving only, Book the Cra Now</p>
+                <button className="mt-5 bg-blue-400 h-10 px-4 rounded-full flex justify-center items-center hover:bg-blue-500 cursor-pointer font-bold ">
+                    Explore Cars
+                    </button>
+            </div>
+            
+            <div>
+                
+            </div>
+        </div>
     );
 }
