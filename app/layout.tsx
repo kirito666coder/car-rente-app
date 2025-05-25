21053e5debe2571300a3e2790fe901cd5d0b9516
kirito <kirito666codr@gmail.com>
2025-05-25 18:47:00 +0530

    Wrap layout in ClerkProvider and add authentication header

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,12 +31,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
         {children}
       </body>
     </html>
+        </ClerkProvider>
   );
 }
