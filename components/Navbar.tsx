2f193f68a59808e61bdd4524a91db9eea5b4dc9c
kirito <kirito666codr@gmail.com>
2025-05-25 19:46:09 +0530

    refactor: clean up Navbar component structure for better readability

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,32 +2,53 @@ import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 export default function Navbar() {
-    return (
-        <nav className="w-screen flex justify-between items-center border-b-[1px] shadow-md shadow-white/50">
-            {/* logo */}
-          <div className=" ml-5 md:ml-15">
-            <Image
-            src={'/logo.png'}
-            alt="logo"
-            height={120}
-            width={120}
-            
-            />
-          </div>
-            {/* menu for big screens */}
-          <div className="mr-15 hidden md:flex justify-center items-center gap-4">
-            <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/"} >Home</Link>
-            <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/history"} >History</Link>
-            <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/contact"} >Contact Us</Link>
+  return (
+    <nav className="w-screen flex justify-between items-center border-b-[1px] shadow-md shadow-white/50">
+      {/* logo */}
+      <div className=" ml-5 md:ml-15">
+        <Image
+          src={'/logo.png'}
+          alt="logo"
+          height={120}
+          width={120}
 
-            <UserButton/>
-          </div>
-          {/* menu for small screens */}
-          <div className="flex md:hidden">
+        />
+      </div>
+      {/* menu for big screens */}
+      <div className="mr-15 hidden md:flex justify-center items-center gap-4">
+        <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/"} >Home</Link>
+        <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/history"} >History</Link>
+        <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/contact"} >Contact Us</Link>
 
-          </div>
-         
-        </nav>
-    );
+        <UserButton />
+      </div>
+      {/* menu for small screens */}
+      <div className="flex md:hidden">
+        <Sheet>
+          <SheetTrigger>Open</SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+
+      </div>
+
+    </nav>
+  );
 }
