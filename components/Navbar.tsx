827f18f751cce0ec74f71139afb4eab10f69da30
kirito <kirito666codr@gmail.com>
2025-05-25 19:06:18 +0530

    Refactor Navbar component to remove unused UserButton and add logo structure

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,19 @@
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Navbar() {
     return (
-        <div>
-
-
-            <UserButton/>
-        </div>
+        <nav>
+            {/* logo */}
+          <div>
+            <Image
+            src={}
+            alt="logo"
+            height={32}
+            width={32}
+            />
+          </div>
+        
+        </nav>
     );
 }
