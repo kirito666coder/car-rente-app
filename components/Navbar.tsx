b2604d2347cac6bab9f79d7a59acf1c0ce431276
kirito <kirito666codr@gmail.com>
2025-05-25 20:05:12 +0530

    feat: add ModeToggle component to Navbar for improved theme switching

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
@@ -31,38 +32,40 @@ export default function Navbar() {
         <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/contact"} >Contact Us</Link>
 
         <UserButton />
+        <ModeToggle />
       </div>
       {/* menu for small screens */}
       <div className="mr-5 flex md:hidden gap-4">
         <Sheet>
           <SheetTrigger><svg
-  width="24"
-  height="24"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="currentColor"
-  strokeWidth="2"
-  strokeLinecap="round"
-  strokeLinejoin="round"
-  className="text-black"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <line x1="3" y1="6" x2="21" y2="6" />
-  <line x1="3" y1="12" x2="21" y2="12" />
-  <line x1="3" y1="18" x2="21" y2="18" />
-</svg>
-</SheetTrigger>
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="text-black"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line x1="3" y1="6" x2="21" y2="6" />
+            <line x1="3" y1="12" x2="21" y2="12" />
+            <line x1="3" y1="18" x2="21" y2="18" />
+          </svg>
+          </SheetTrigger>
           <SheetContent className="w-70">
             <div className="flex flex-col justify-center items-center mt-5 gap-4">
-            <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/"} >Home</Link>
-            <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/history"} >History</Link>
-            <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/contact"} >Contact Us</Link>
+              <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/"} >Home</Link>
+              <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/history"} >History</Link>
+              <Link className="cursor-pointer relative hover:text-yellow-400 after:absolute after:content-empty after:w-0 hover:after:w-full after:h-[2px] after:transition-all after:duration-300 after:bottom-0 after:left-0 after:bg-yellow-300" href={"/contact"} >Contact Us</Link>
             </div>
 
           </SheetContent>
         </Sheet>
 
         <UserButton />
+        <ModeToggle />
       </div>
 
     </nav>
