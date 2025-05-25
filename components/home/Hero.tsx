e0e6d320408d406e09205716b06adc7ce19069f2
kirito <kirito666codr@gmail.com>
2025-05-25 23:44:03 +0530

    feat: update button color in Hero component for better visibility

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
             <div className="md:mt-55 md:ml-20">
                 <h1 className="font-semibold text-5xl">Premium Car rental in your Area</h1>
                 <p className="mt-10 text-gray-500 font-semibold text-2xl max-w-[90%]">Book the selected car effortiessly, Pay for driving only, Book the Cra Now</p>
-                <button className="mt-5 bg-blue-400 h-10 px-4 rounded-full flex justify-center items-center hover:bg-blue-500 cursor-pointer font-bold ">
+                <button className="mt-5 bg-yellow-500/95 h-10 px-4 rounded-full flex justify-center items-center hover:bg-yellow-500  hover:scale-102 cursor-pointer font-bold ">
                     Explore Cars
                     </button>
             </div>
