73eff6ec3e12d46bfcf19cc7fa14e31e4e878529
kirito <kirito666codr@gmail.com>
2025-05-25 19:27:08 +0530

    Refactor code structure for improved readability and maintainability

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,25 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="flex ">
+        <nav className="w-screen flex justify-between items-center">
             {/* logo */}
-          <div>
+          <div className="ml-5">
             <Image
-            src={'/car-rente-logo.png'}
+            src={'/logo.png'}
             alt="logo"
             height={120}
             width={120}
-            className="invert"
+            
             />
           </div>
             {/* menu for big screens */}
-          <div>
-
+          <div className="mr-5 flex justify-center items-center gap-4">
+            <Link href={"/"} >Home</Link>
+            <Link href={"/history"} >History</Link>
+            <Link href={"/contact"} >Contact Us</Link>
 
             <UserButton/>
           </div>
