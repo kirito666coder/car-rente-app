8059f0e03ae94aebdfed6f9f9724eef59f6ca48d
kirito <kirito666codr@gmail.com>
2025-05-26 18:19:18 +0530

    add: implement initial layout for CarsCatalog component

--- components/home/CarfillterSection.tsx
+++ components/home/CarfillterSection.tsx
@@ -1,5 +1,12 @@
 export default function CarsCatalog() {
     return (
-        <div></div>
+        <div className="ml-10 md:ml-40">
+            <h3 className="text-2xl font-bold mt-10 ">Cars Catalog</h3>
+            <p>Explore our cars you might likes</p>
+
+            <div>
+                
+            </div>
+        </div>
     );
 }
