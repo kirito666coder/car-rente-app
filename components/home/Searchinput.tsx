76df95aea7aaa9ce8e77be55626a95495f9e9b97
kirito <kirito666codr@gmail.com>
2025-05-26 14:14:07 +0530

    fix: remove unnecessary width class from heading in Searchinput component

--- components/home/Searchinput.tsx
+++ components/home/Searchinput.tsx
@@ -1,7 +1,12 @@
 export default function Searchinput() {
     return (
         <div>
-            <h2 className="text-center w-30 ">Let's Search what you need</h2>
+            <h2 className="text-center ">Let's Search what you need</h2>
+            <div>
+                <div>
+                    <input type="text" placeholder="Location" className="p-2 outline-none " />
+                </div>
+            </div>
         </div>
     );
 }
