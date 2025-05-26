f3e887cda1e79fff80b85a836f9fcdff26c6a703
kirito <kirito666codr@gmail.com>
2025-05-26 23:23:48 +0530

    feat: enhance CarCar component styling and image rendering

--- components/home/CarCar.tsx
+++ components/home/CarCar.tsx
@@ -5,19 +5,21 @@ export default function CarCar(props:any) {
 
     const [Car, setCar] = useState(props.car)
     return (
-        <div>
-            <h2 className="text-[20px]  font-medium mb-2">{Car.name}</h2>
-            <h2 className="text-[20px] font-bold mb-2">
-                <span className="text-[12px] font-light">$</span>
+        <div className="bg-white m-10 text-black p-5 rounded-2xl">
+            <h2 className="text-4xl font-semibold mb-2">{Car.name}</h2>
+            <h2 className="text-3xl font-bold mb-2">
+                <span className="text-[19px] font-medium">$</span>
                 {Car.price}
-                <span className="text-[12px] font-light">\day</span>
+                <span className="text-[19px] font-medium">\day</span>
             </h2>
+            <div className="flex justify-center items-center">
             <Image
-            src={Car.image.url}
+            src={Car.image?.url}
             alt="car-Image"
-            height={25}
-            width={25}
+            height={300}
+            width={300}
             />
+            </div>
         </div>
     );
 }
