5a34e9b7e6b1845d71d97dbf3688e3b78552fbee
kirito <kirito666codr@gmail.com>
2025-05-26 23:04:15 +0530

    feat: enhance CarCar component layout with structured rendering for car details

--- components/home/CarCar.tsx
+++ components/home/CarCar.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from "react";
 
 export default function CarCar(props:any) {
@@ -5,7 +6,18 @@ export default function CarCar(props:any) {
     const [Car, setCar] = useState(props.car)
     return (
         <div>
-            {Car.name}
+            <h2 className="text-[20px]  font-medium mb-2">{Car.name}</h2>
+            <h2 className="text-[20px] font-bold mb-2">
+                <span className="text-[12px] font-light">$</span>
+                {Car.price}
+                <span className="text-[12px] font-light">\day</span>
+            </h2>
+            <Image
+            src={Car.image.url}
+            alt="car-Image"
+            height={25}
+            width={25}
+            />
         </div>
     );
 }
