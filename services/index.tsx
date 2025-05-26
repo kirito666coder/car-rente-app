b535669eeea18f1f0a81eb5f1ccd272752afa086
kirito <kirito666codr@gmail.com>
2025-05-26 22:53:19 +0530

    fix: adjust query formatting and ensure image URL is included in car data retrieval

--- services/index.tsx
+++ services/index.tsx
@@ -3,7 +3,7 @@ import { gql, request } from 'graphql-request'
 
 export default async function getCarsList() {
     const query = gql`
-    query MyQuery {
+  query MyQuery {
   carLists {
     carAvg
     carBrand
@@ -14,6 +14,9 @@ export default async function getCarsList() {
     updatedAt
     price
     name
+    image {
+      url
+    }
   }
 }`
 
