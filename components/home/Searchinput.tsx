a563da8b5d13a58d881a030b642b9d6d82c4e6cb
kirito <kirito666codr@gmail.com>
2025-05-26 17:58:43 +0530

    fix: adjust layout and styling in Searchinput component

--- components/home/Searchinput.tsx
+++ components/home/Searchinput.tsx
@@ -2,9 +2,9 @@ export default function Searchinput() {
     return (
         <div>
             <h2 className="text-center text-gray-500">Let's Search what you need</h2>
-            <div>
+            <div className="flex justify-center items-center">
                 
-                <div className="flex items-center text-gray-400 m-4 border-b-gray-400 border-b-2">
+                <div className="flex items-center  m-4 border-b-gray-400 border-b-2">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="25" height="25">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                             d="M12 11.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
@@ -13,6 +13,10 @@ export default function Searchinput() {
                     </svg>
 
                     <input type="text" placeholder="Location" className="p-2 outline-none " />
+                </div>
+
+                <div className="flex items-center text-gray-400 m-4 border-b-gray-400 border-b-2">
+                    <input type="date" placeholder="Location" className="p-2 outline-none " />
                 </div>
             </div>
         </div>
