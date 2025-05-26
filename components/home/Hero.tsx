7bdf9deca7b4786c5350dee90274631843ea5d3f
kirito <kirito666codr@gmail.com>
2025-05-26 13:59:27 +0530

    fix: correct margin and heading size in Hero component for better layout

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Hero() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 ml-10 md:ml-20 mt-20 ">
-            <div className="md:mt-55 md:ml-20">
-                <h1 className="font-semibold text-5xl">Premium Car rental in your Area</h1>
-                <p className="mt-10 text-gray-500 font-semibold text-2xl max-w-[90%]">Book the selected car effortiessly, Pay for driving only, Book the Cra Now</p>
+            <div className="md:mt-50 md:ml-20">
+                <h1 className="font-semibold text-5xl md:text-7xl">Premium Car rental in your Area</h1>
+                <p className="mt-8 text-gray-500 font-semibold text-2xl max-w-[90%]">Book the selected car effortiessly, Pay for driving only, Book the Cra Now</p>
                 <button className="mt-5 bg-yellow-500/95 h-10 px-4 rounded-full flex justify-center items-center hover:bg-yellow-500  hover:scale-102 cursor-pointer font-bold ">
                     Explore Cars
                     </button>
