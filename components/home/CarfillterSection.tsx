d62990b262d94587e83e2764cce0a3b896b5e514
kirito <kirito666codr@gmail.com>
2025-05-26 18:52:21 +0530

    fix: enhance layout and update dropdown menu labels in CarsCatalog component

--- components/home/CarfillterSection.tsx
+++ components/home/CarfillterSection.tsx
@@ -9,41 +9,40 @@ import {
 
 export default function CarsCatalog() {
     return (
-        <div className="ml-10 md:ml-40">
+        <div className="ml-10 md:ml-40 flex justify-between items-center">
+            <div>
             <h3 className="text-2xl font-bold mt-10 ">Cars Catalog</h3>
             <p>Explore our cars you might likes</p>
+            </div>
 
-            <div>
+            <div className="flex gap-2 mt-12 mr-10">
                 <DropdownMenu>
-                    <DropdownMenuTrigger className="flex justify-center items-center gap-1 ">Price
+                    <DropdownMenuTrigger className="border-2 px-2 py-1 rounded-2xl flex justify-center items-center gap-1 ">Price
                         <svg className="mt-0.5" width="13" height="13" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
                             <polygon points="25,45 5,5 45,5" fill="currentColor" />
                         </svg>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuLabel>Price</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                        <DropdownMenuItem>Min to Max</DropdownMenuItem>
+                        <DropdownMenuItem>Max to Min</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
 
 
                 <DropdownMenu>
-                    <DropdownMenuTrigger className="flex justify-center items-center gap-1 ">Manefoctur
+                    <DropdownMenuTrigger className="border-2 px-2 py-1 rounded-2xl flex justify-center items-center gap-1 ">Manufacture
                         <svg className="mt-0.5" width="13" height="13" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
                             <polygon points="25,45 5,5 45,5" fill="currentColor" />
                         </svg>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuLabel>Manufactural</DropdownMenuLabel>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                        <DropdownMenuItem>Honda</DropdownMenuItem>
+                        <DropdownMenuItem>BMW</DropdownMenuItem>
+                        <DropdownMenuItem>Toyota</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
