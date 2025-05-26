de6d9ca65c1ff7fcdc41f9ee0d2930c3bd13f39b
kirito <kirito666codr@gmail.com>
2025-05-26 22:21:52 +0530

    feat: implement car data fetching in Home component using getCarsList

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+"use client"
 import CarsCatalog from "@/components/home/CarfillterSection";
 import Hero from "@/components/home/Hero";
 import Searchinput from "@/components/home/Searchinput";
+import getCarsList from "@/services";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+const [CarList, setCarList] = useState()
+
+ const getcarsList = async()=>{
+  const result:any = await getCarsList()
+  console.log(result)
+  setCarList(result)
+ }
+
+ useEffect(() => {
+getcarsList()
+ }, [])
+ 
+
   return (
     <main>
     <Hero/>
