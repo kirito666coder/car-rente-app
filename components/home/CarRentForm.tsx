e0b0055949734a9cad9eb192cb9d7b2f549142e4
kirito <kirito666codr@gmail.com>
2025-05-28 19:50:39 +0530

    feat: integrate DatePickerWithRange into CarRentForm for enhanced date selection

--- components/home/CarRentForm.tsx
+++ components/home/CarRentForm.tsx
@@ -1,9 +1,12 @@
 import { ComboboxDemo } from "../ui/combobox";
+import { DatePickerWithRange } from "../ui/DateRangePicker";
+
 
 export default function CarRentForm() {
     return (
         <div>
             <ComboboxDemo/>
+            <DatePickerWithRange/>
         </div>
     );
 }
