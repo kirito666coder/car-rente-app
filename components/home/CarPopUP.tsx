c2dbfdb026daff5bd0fd7a61b643a06ccaa93a3c
kirito <kirito666codr@gmail.com>
2025-05-28 18:43:49 +0530

    feat: implement CarPOPup component with Drawer for car rental interaction

--- components/home/CarPopUP.tsx
+++ components/home/CarPopUP.tsx
@@ -1,5 +1,40 @@
+import { Button } from "../ui/button";
+import CarCar from "./CarCar";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
 export default function CarPOPup() {
     return (
-        <div></div>
+        <Drawer>
+            <DrawerTrigger>
+                <div>
+                    <CarCar car={car} />
+                </div>
+            </DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Rent A Car Now!</DrawerTitle>
+                    <DrawerDescription className="grid grid-cols-1 md:grid-cols-2">
+                        <div>
+                            <CarCar car={car} />
+                        </div>
+                        <div></div>
+                    </DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                    <Button>Submit</Button>
+                    <DrawerClose>
+                        <Button variant="outline">Cancel</Button>
+                    </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
     );
 }
