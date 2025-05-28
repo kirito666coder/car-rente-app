6514287c0776119dde67ae84348ced311309cefd
kirito <kirito666codr@gmail.com>
2025-05-28 16:26:20 +0530

    fix: update CarsLists component to use car.id as key in map function

--- components/home/CarsLists.tsx
+++ components/home/CarsLists.tsx
@@ -1,13 +1,13 @@
 import CarCar from "./CarCar";
 
 export default function CarsLists(props:any) {
-
+      console.log(props)
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 ">
             {
                 props.CarList.map((car:any,index:number)=>{
 
-                 return(<div key={index}>
+                 return(<div key={car.id}>
                     <CarCar car={car}/>
                  </div>)
                 })
