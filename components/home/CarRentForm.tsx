835808ad29dfff143e73a6a12a361761f9f0b3d5
kirito <kirito666codr@gmail.com>
2025-05-28 23:45:06 +0530

    feat: adjust spacing in CarRentForm and enhance DrawerHeader overflow behavior

--- components/home/CarRentForm.tsx
+++ components/home/CarRentForm.tsx
@@ -7,12 +7,13 @@ export default function CarRentForm() {
         <div className="md:mt-20 text-lg">
             <h3>PickUp Location</h3>
             <ComboboxDemo/>
-            <div className="flex justify-between items-center w-[90%] mt-2">
+            <div className="flex justify-between items-center w-[90%] mt-5">
                 <div>Pick Up Date</div>
                 <div>Pick Off Date</div>
             </div>
             <DatePickerWithRange/>
             <h3 className="mt-5">Contact Number</h3>
+            <input type="tel" placeholder="Contact Number" className="outline-none border-gray-300 border-1 py-2 text-sm w-[90%] rounded-sm px-4 placeholder:font-medium" />
         </div>
     );
 }
