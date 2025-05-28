1000916e4c59e81d8708d9323e9ac76cec74ceba
kirito <kirito666codr@gmail.com>
2025-05-28 23:19:47 +0530

    feat: enhance layout of CarRentForm with date selection headers

--- components/home/CarRentForm.tsx
+++ components/home/CarRentForm.tsx
@@ -4,9 +4,15 @@ import { DatePickerWithRange } from "../ui/DateRangePicker";
 
 export default function CarRentForm() {
     return (
-        <div>
+        <div className="md:mt-20 text-lg">
+            <h3>PickUp Location</h3>
             <ComboboxDemo/>
+            <div className="flex justify-between">
+                <div>Pick Up Date</div>
+                <div>Pick Off Date</div>
+            </div>
             <DatePickerWithRange/>
+            <h3>Contact Number</h3>
         </div>
     );
 }
