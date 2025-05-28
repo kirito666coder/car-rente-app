e7e9c893ee453da4485b26e149bb948140997ee3
kirito <kirito666codr@gmail.com>
2025-05-28 23:25:28 +0530

    feat: adjust ComboboxDemo dimensions for improved layout

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -42,7 +42,7 @@ export function ComboboxDemo() {
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-[90%] justify-between"
         >
           {value
             ? frameworks.find((framework) => framework.value === value)?.label
@@ -50,7 +50,7 @@ export function ComboboxDemo() {
           <ChevronsUpDown className="opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-[300px] p-0">
         <Command>
           {/* <CommandInput placeholder="Search framework..." className="h-9" /> */}
           <CommandList>
