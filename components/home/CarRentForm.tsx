fd40b18765693df4877c996cb6ae760c0e96869c
kirito <kirito666codr@gmail.com>
2025-05-29 16:37:42 +0530

    refactor: streamline contact number state management in CarRentForm component

--- components/home/CarRentForm.tsx
+++ components/home/CarRentForm.tsx
@@ -5,36 +5,38 @@ import { DatePickerWithRange } from "../ui/DateRangePicker";
 
 export default function CarRentForm() {
     const [Formdata, setFormdata] = useState({
-        location:"",
-        pickUpDate:"",
-        PickOffDate:"",
-        ContactNumber:"",
-})
+        location: "",
+        pickUpDate: "",
+        PickOffDate: "",
+        ContactNumber: "",
+    })
 
-useEffect(() => {
-  console.log(Formdata)
-}, [Formdata])
+    useEffect(() => {
+        console.log(Formdata)
+    }, [Formdata])
 
 
-const [ContactNumber, setContactNumber] = useState('')
     return (
         <div className="md:mt-20 text-lg">
             <h3>PickUp Location</h3>
-            <ComboboxDemo setFormdata={setFormdata}/>
+            <ComboboxDemo setFormdata={setFormdata} />
             <div className="flex justify-between items-center w-[90%] mt-5">
                 <div>Pick Up Date</div>
                 <div>Pick Off Date</div>
             </div>
-            <DatePickerWithRange setFormdata={setFormdata}/>
+            <DatePickerWithRange setFormdata={setFormdata} />
             <h3 className="mt-5">Contact Number</h3>
             <input
-             value={ContactNumber}
-             onChange={(e)=>{
-             setContactNumber(e.target.value)
-             }}
-             type="tel"
-            placeholder="Contact Number"
-             className="outline-none border-gray-300 border-1 py-2 text-sm w-[90%] rounded-sm px-4 placeholder:font-medium" />
+                value={Formdata.ContactNumber}
+                onChange={(e) => {
+                    setFormdata((prev) => ({
+                        ...prev,
+                        ContactNumber: e.target.value,
+                    }))
+                }}
+                type="tel"
+                placeholder="Contact Number"
+                className="outline-none border-gray-300 border-1 py-2 text-sm w-[90%] rounded-sm px-4 placeholder:font-medium" />
         </div>
     );
 }
